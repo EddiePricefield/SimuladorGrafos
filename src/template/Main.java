eb8398a6881debb39993afe7b1f3e832c3d588ec
EddiePricefield <eduardofsantana17@gmail.com>
2025-11-28 20:22:47 -0300

    Implementei uma versão base para um criador de Grafos

--- src/template/Main.java
+++ src/template/Main.java
@@ -4,9 +4,11 @@ import br.com.davidbuzatto.jsge.collision.CollisionUtils;
 import br.com.davidbuzatto.jsge.core.Camera2D;
 import br.com.davidbuzatto.jsge.core.engine.EngineFrame;
 import static br.com.davidbuzatto.jsge.core.engine.EngineFrame.GRAY;
+import br.com.davidbuzatto.jsge.imgui.GuiButton;
 import br.com.davidbuzatto.jsge.imgui.GuiButtonGroup;
 import br.com.davidbuzatto.jsge.imgui.GuiComponent;
 import br.com.davidbuzatto.jsge.imgui.GuiLabelButton;
+import br.com.davidbuzatto.jsge.imgui.GuiSpinner;
 import br.com.davidbuzatto.jsge.imgui.GuiToggleButton;
 import br.com.davidbuzatto.jsge.math.Vector2;
 import java.awt.Desktop;
@@ -32,10 +34,14 @@ public class Main extends EngineFrame {
     //Componentes
     private List<GuiComponent> componentes;
     private GuiButtonGroup buttonGroup;
-    private GuiToggleButton btnSelecionarGrafo;
-    private GuiToggleButton btnAdicionarGrafo;
-    private GuiToggleButton btnAdicionarAresta;
+    private GuiToggleButton btnSelecionarVertice;
+    private GuiToggleButton btnAdicionarVertice;
+    private GuiButton btnAdicionarAresta;
+    private GuiButton btnRemoverVertice;
+    private GuiSpinner spinnerVerticeDestino;
     
+    private boolean mouseForaDaIMGUI;
+            
     private GuiLabelButton btnLink;
     
     //Câmera
@@ -46,6 +52,7 @@ public class Main extends EngineFrame {
     //Grafo
     private Grafo grafo;
     private int verticeAtual;
+    private boolean verticeClicado;
     
     public Main() {
         
@@ -73,6 +80,7 @@ public class Main extends EngineFrame {
         camera = new Camera2D();
         componentes = new ArrayList<>();
         buttonGroup = new GuiButtonGroup();
+        grafo = new Grafo();
         
         //Criação da Câmera
         cameraPos = new Vector2(0, 0);
@@ -82,44 +90,14 @@ public class Main extends EngineFrame {
         camera.zoom = 1;
         
         //Criação dos Componentes
-        btnSelecionarGrafo = new GuiToggleButton(25, 25, 120, 30, "Selecionar");
-        btnAdicionarGrafo = new GuiToggleButton(175, 25, 120, 30, "Adicionar Grafo");
-        btnAdicionarAresta = new GuiToggleButton(325, 25, 120, 30, "Adicionar Aresta");
-        btnSelecionarGrafo.setButtonGroup(buttonGroup);
-        btnAdicionarGrafo.setButtonGroup(buttonGroup);
-        btnAdicionarAresta.setButtonGroup(buttonGroup);
+        btnSelecionarVertice = new GuiToggleButton(25, 25, 120, 30, "Selecionar");
+        btnAdicionarVertice = new GuiToggleButton(175, 25, 120, 30, "Adicionar Vértice");
+        btnSelecionarVertice.setButtonGroup(buttonGroup);
+        btnAdicionarVertice.setButtonGroup(buttonGroup);
+        btnAdicionarAresta = new GuiButton(325, 25, 120, 30, "Adicionar Aresta");
+        btnRemoverVertice = new GuiButton(475, 25, 120, 30, "Remover Vertice");
+        spinnerVerticeDestino = new GuiSpinner(615, 25, 100, 30, 0, 0, 0);
         
-        grafo = new Grafo();
-        
-        grafo.addVertice(100, 100);
-        grafo.addVertice(150, 200);
-        grafo.addVertice(250, 200);
-        grafo.addVertice(200, 300);
-        grafo.addVertice(300, 300);
-        grafo.addVertice(100, 400);
-        grafo.addVertice(350, 200);
-        grafo.addVertice(450, 150);
-        grafo.addVertice(550, 150);
-        grafo.addVertice(450, 250);
-        grafo.addVertice(550, 250);
-        grafo.addVertice(450, 350);
-        grafo.addVertice(550, 350);
-
-        grafo.addAresta(0, 5);
-        grafo.addAresta(4, 3);
-        grafo.addAresta(0, 1);
-        grafo.addAresta(9, 12);
-        grafo.addAresta(6, 4);
-        grafo.addAresta(5, 4);
-        grafo.addAresta(0, 2);
-        grafo.addAresta(11, 12);
-        grafo.addAresta(9, 10);
-        grafo.addAresta(0, 6);
-        grafo.addAresta(7, 8);
-        grafo.addAresta(9, 11);
-        grafo.addAresta(5, 3);
-        
-        System.out.println(grafo);
         setDefaultFontSize(20);
         setDefaultStrokeLineWidth(2);
         setDefaultStrokeEndCap(STROKE_CAP_ROUND);
@@ -128,9 +106,11 @@ public class Main extends EngineFrame {
         
         //Adicionar à Lista de Componentes
         componentes.add(btnLink);
-        componentes.add(btnSelecionarGrafo);
-        componentes.add(btnAdicionarGrafo);
+        componentes.add(btnSelecionarVertice);
+        componentes.add(btnAdicionarVertice);
         componentes.add(btnAdicionarAresta);
+        componentes.add(btnRemoverVertice);
+        componentes.add(spinnerVerticeDestino);
         
     }
     
@@ -141,11 +121,14 @@ public class Main extends EngineFrame {
         
         atualizarComponentes(delta);
         
+        //Opções Principais
         Vector2 mousePos = camera.getScreenToWorld(getMousePositionPoint());
         
-        if (isMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+        if (isMouseButtonPressed(MOUSE_BUTTON_LEFT) && mouseForaDaIMGUI) {
             
-            if (btnSelecionarGrafo.isSelected() && !btnSelecionarGrafo.isMousePressed()){
+            if (btnSelecionarVertice.isSelected()){
+                
+                verticeClicado = false;
                 
                 for (var vertice : grafo.vertices.values()) {
                     Vector2 centro = new Vector2(vertice.pos.x, vertice.pos.y);
@@ -153,21 +136,34 @@ public class Main extends EngineFrame {
                     if (CollisionUtils.checkCollisionPointCircle(mousePos, centro, 30)) {
                         vertice.corVertice = RED;
                         verticeAtual = vertice.id;
+                        verticeClicado = true;
                         System.out.println(verticeAtual);
                     } else{
                         vertice.corVertice = WHITE;
-                        verticeAtual = -1;
                     }
 
                 }
                 
+                if(!verticeClicado){
+                    verticeAtual = -1;
+                }
+                
             }
             
-            if (btnAdicionarGrafo.isSelected() && !btnAdicionarGrafo.isMousePressed()){ //Tive que fazer assim pra ele nao desenhar Vertice embaixo do botão, ja reproduzi pros outros botões também
+            if (btnAdicionarVertice.isSelected() && !btnAdicionarVertice.isMousePressed()){
                 grafo.addVertice(mousePos.x, mousePos.y);
             }
-            
-            
+
+        }
+        
+        //Opções Extras
+        btnAdicionarAresta.setEnabled(verticeClicado);
+        btnRemoverVertice.setEnabled(verticeClicado);
+        spinnerVerticeDestino.setEnabled(verticeClicado);
+        spinnerVerticeDestino.setMax(grafo.getQuantidadeVertices() - 1);
+        
+        if (btnAdicionarAresta.isMousePressed()){
+            grafo.addAresta(verticeAtual, spinnerVerticeDestino.getValue());
         }
         
         //Github
@@ -206,10 +202,16 @@ public class Main extends EngineFrame {
     //----------< Complementares >----------//    
     
     public void atualizarComponentes(double delta) {
+        
+        mouseForaDaIMGUI = true;
 
         for (GuiComponent c : componentes) {
             if (!componentes.isEmpty()) {
                 c.update(delta);
+            }
+            
+            if (c.isMousePressed()){
+                mouseForaDaIMGUI = false;
             }
         }
 
